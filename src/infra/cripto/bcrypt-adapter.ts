e1d7b79609abc9e887ed5962cf0205e7d5e8304e
kaiorr <kaio.ribeiro@sistemavitto.com.br>
2021-05-15 12:19:20 -0300

    feat: ensure BcryptAdapter renturns a hash on success

--- src/infra/cripto/bcrypt-adapter.ts
+++ src/infra/cripto/bcrypt-adapter.ts
@@ -2,14 +2,14 @@ import { Encrypter } from '../../data/protocols/encrypter'
 import bcrypt from 'bcrypt'
 
 export class BcryptAdapter implements Encrypter {
-  private readonly salt
+  private readonly salt: number
 
   constructor (salt: number) {
     this.salt = salt
   }
 
   async encrypt (value: string): Promise<string> {
-    await bcrypt.hash(value, 12)
-    return null
+    const hash = await bcrypt.hash(value, this.salt)
+    return hash
   }
 }
